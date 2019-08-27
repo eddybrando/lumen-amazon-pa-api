875862d479a5e8a7e3411d686cb4e925809f7e27
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-08-27 07:20:32 +0200

    feat: create "item-lookup" route

---
+++ app/Http/Controllers/LookupController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class LookupController extends Controller
+{
+    public function item()
+    {
+        // TODO: Handle item lookup
+        return response()->json(["foo" => "bar"]);
+    }
+}
